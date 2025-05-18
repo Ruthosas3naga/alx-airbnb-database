07281a8830092115aff730779b215d9974a5c109
osasenaga <edionwe.osasenaga5@gmail.com>
2025-05-18 20:43:02 +0100

    added optimization and performance sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,24 +1,29 @@
 
 
 -- Initial complex query
+-- performance.sql
+
+-- Initial complex query (with AND in WHERE clause)
 SELECT 
     b.id AS booking_id,
     b.start_date,
     b.end_date,
     b.status,
-    u.id AS user_id,
     u.name AS user_name,
     u.email,
-    p.id AS property_id,
     p.title AS property_title,
     p.location,
-    pay.id AS payment_id,
     pay.amount,
     pay.payment_date
 FROM Booking b
 JOIN User u ON b.user_id = u.id
 JOIN Property p ON b.property_id = p.id
-LEFT JOIN Payment pay ON pay.booking_id = b.id;
+LEFT JOIN Payment pay ON pay.booking_id = b.id
+WHERE b.status = 'confirmed'
+  AND b.start_date >= CURRENT_DATE
+ORDER BY b.start_date DESC
+LIMIT 100;
+
 
 "Analyse with EXPLAIN"
 EXPLAIN ANALYZE
